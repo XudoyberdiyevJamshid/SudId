cf7361187b9bc4678be0ca5d5d39b496be1beeb4
Jamshid Xudoyberdiyev <jamshiddev1@mail.com>
2026-03-05 11:56:05 +0500

    Refactor code structure for improved readability and maintainability

--- src/app/features/settings/pages/login-history/login-history.ts
+++ src/app/features/settings/pages/login-history/login-history.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { GCard } from '../../../../shared/components/g-card/g-card';
+import { NoData } from '../../../../shared/ui/no-data/no-data';
 
 @Component({
   selector: 'app-login-history',
-  imports: [],
+  imports: [GCard, NoData],
   templateUrl: './login-history.html',
   styleUrl: './login-history.scss',
 })
-export class LoginHistory {
-
-}
+export class LoginHistory {}
--- src/app/features/settings/pages/manage-devices/manage-devices.ts
+++ src/app/features/settings/pages/manage-devices/manage-devices.ts
@@ -1,11 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { GIcon } from '../../../../shared/components/g-icon/g-icon';
+import { GCard } from '../../../../shared/components/g-card/g-card';
+import { NoData } from '../../../../shared/ui/no-data/no-data';
+
+export interface Session {
+  id: string;
+  type: 'laptop' | 'smartphone';
+  name: string;
+  lastActive: string;
+  isCurrent: boolean;
+}
 
 @Component({
   selector: 'app-manage-devices',
-  imports: [],
+  imports: [GIcon, GCard, NoData],
   templateUrl: './manage-devices.html',
   styleUrl: './manage-devices.scss',
 })
 export class ManageDevices {
+  router = inject(Router);
 
+  allSessions = signal<Session[]>([
+    { id: '1', type: 'laptop', name: 'PC-Windows-11', lastActive: '15.10.2025', isCurrent: true },
+    { id: '2', type: 'laptop', name: 'PC-Windows-11', lastActive: '15.10.2025', isCurrent: false },
+    { id: '3', type: 'laptop', name: 'Laptop-MacOS', lastActive: '20.10.2025', isCurrent: false },
+    {
+      id: '4',
+      type: 'smartphone',
+      name: 'Tablet-Android',
+      lastActive: '25.10.2025',
+      isCurrent: false,
+    },
+  ]);
+
+  activeSession = computed(() => this.allSessions().filter((s) => s.isCurrent));
+  otherSessions = computed(() => this.allSessions().filter((s) => !s.isCurrent));
+
+  isModalOpen = signal(false);
+  selectedSession = signal<Session | null>(null);
+
+  goBack() {
+    this.router.navigate(['/dashboard/settings']);
+  }
+
+  openModal(session: Session) {
+    this.selectedSession.set(session);
+    this.isModalOpen.set(true);
+  }
+
+  closeModal() {
+    this.isModalOpen.set(false);
+    this.selectedSession.set(null);
+  }
+
+  confirmCloseSession() {
+    const sessionToRemove = this.selectedSession();
+    if (sessionToRemove) {
+      this.allSessions.update((sessions) => sessions.filter((s) => s.id !== sessionToRemove.id));
+    }
+    this.closeModal();
+  }
 }
