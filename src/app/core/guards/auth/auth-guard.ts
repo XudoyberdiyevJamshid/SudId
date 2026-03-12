1b90c6a15510104c817e93f9928dd56547b0faa5
Jamshid Xudoyberdiyev <jamshiddev1@mail.com>
2026-03-12 15:37:22 +0500

    Redirect unauthenticated users to the authorization URL instead of navigating to login

--- src/app/core/guards/auth/auth-guard.ts
+++ src/app/core/guards/auth/auth-guard.ts
@@ -10,7 +10,12 @@ export const authGuard: CanActivateFn = (route, state) => {
     return true;
   }
 
-  router.navigate(['/login']);
+  const clientId = '1';
+  const redirectUri = encodeURIComponent('http://localhost:4200/login');
+  const authUrl = `http://localhost:9000/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=openid%20profile`;
+
+  // 3. Tashqi URL ga yo'naltirish (GET so'rovi avtomatik bajariladi)
+  window.location.href = authUrl;
 
   return false;
 };
